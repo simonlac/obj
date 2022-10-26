126d1b88f0111b003a5abd7e697d21adf8a2215f
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:57:22 -0400

    Add files via upload

---
+++ Classes_Objets/Exemple_cours/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple_cours
+{
+    // Définir la classe Person.
+    public class Person
+    {
+        //Déclarer les attributs privés.
+        private string nom;
+        private string prenom;
+        private string ville;
+
+        // Déclarer les propriétés.
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+
+        }
+
+        // Définir le constructeur sans paramètres de la classe Person.
+        public Person()
+        {
+            Console.WriteLine("Création d'une nouvelle personne.");
+
+        }
+
+        // Définir le constructeur avec deux paramètres - Surcharger le constructeur-.
+        public Person(string p_nom, string p_prenom)
+        {
+            Console.WriteLine("Création d'une nouvelle personne qui se nomme "
+                    + p_prenom + " " + p_nom+".");
+        }
+
+        // Définir un constructeur avec trois paramètres - Surcharger le constructeur-.
+        public Person(string p_nom, string p_prenom, string p_ville)
+        {
+            this.nom = p_nom;
+            this.prenom = p_nom;
+            this.ville = p_ville;
+            Console.WriteLine("Création d'une nouvelle personne qui se nomme "
+               + p_prenom + " " + p_nom + " et qui habite à " + p_ville+".");
+        }
+        // Définir le destructeur de la classe Person.
+        ~Person()
+        {
+            Console.WriteLine("Destruction d'un objet de la classe Person.");
+            Console.ReadKey();
+        }
+
+
+    }
+
+}
---
+++ Classes_Objets/Exemple_cours/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple_cours
+{
+    class Program
+    {
+                static void Main(string[] args)
+        {
+            // Déclarer l'objet. 
+            Person hasna;
+            // Instancier l'objet. 
+            hasna = new Person();
+            
+            // Déclarer et instancier l'objet Daniel. 
+            Person daniel = new Person();
+            
+
+            // Déclarer et instantier l'objet Patrick avec un constructeur à deux paramètres.
+            Person patrick = new Person("Roy", "Patrick");
+
+            // Déclarer et instancier au moyen d'un constructeur à trois paramètres.
+            Person isabelle = new Person("Roy", "Isabelle","Gatineau");
+            
+            // Modifier l'attribut nom au moyen de la propriété Nom (Setter).
+            Console.WriteLine("Le nom de famille d'Isabelle est "+isabelle.Nom+".");
+            // Propriété Nom, Setter
+            isabelle.Nom = "Couillard";
+            // Afficher l'attribut nom au moyen de la propriété Nom (Getter).
+            Console.WriteLine("Après la modification de la propriété Nom, " +
+                "le nom de famille d'Isabelle est devenu "+isabelle.Nom+".");
+            Console.ReadKey();
+
+
+        }
+    }
+}
